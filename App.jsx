ad2c94fbdc4da1eefb9388589964e2fdb84eb147
Thanh nguyen <thanh.hispvietnam@gmail.com>
2017-11-22 13:47:06 +0700

    add

--- App.jsx
+++ App.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import Hello from './Hello.jsx'
+import {connect} from 'react-redux'
+import {addStudent} from './main.js'
+class App extends React.Component{
 
-export default class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+             name: "Initasdfsdf "
+        }
+    }
+    handleChange(e){
+        var change = {}
+        change[e.target.name] = e.target.value
+        this.setState(change)
+    }
+    add(){
+        this.props.dispatch(addStudent({
+            name: this.state.name,
+            age: this.state.age
+        }))
+    }
+    handleDelete(name){
+        if(confirm('Do you want to delete?')){
+            this.props.dispatch({type: 'DELETE_STUDENT', 
+            payload: name })  
+        }
+    }
     render(){
         return(
         <div>
-           <Hello name='Thanh'/>
-           <Hello name='Hien'/>
+        <input type="text" name='name' value={this.state.name} 
+            onChange={this.handleChange.bind(this)}
+        />
+        <input type="text" name='age' value={this.state.age} 
+            onChange={this.handleChange.bind(this)}
+        />
+        <button onClick={this.add.bind(this)}>Add a student</button>
+
+          {this.props.students.map((s)=>
+                <li>
+                       {s.name} {s.age} 
+                       | <a onClick={()=>this.handleDelete(s.name)}>Delete</a>
+                  </li>
+              
+        )}
         </div>
         )
     }
 }
+function mapStateToProps(centralState){
+    return {
+        students: centralState.students
+    }
+}
+export default connect(mapStateToProps)(App)
--- main.js
+++ main.js
@@ -1,8 +1,105 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import {combineReducers, createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import Test from './Test.jsx'
+
+function students(state = [], action){
+    if(action.type=='LOAD_STUDENT'){
+        console.log(state)
+        return state
+    } else if(action.type=='FETCH_STUDENT_SUCCESS'){
+        return action.payload
+    }
+    else if(action.type=='ADD_STUDENT'){
+        console.log(action.payload)
+        return [...state, action.payload]
+    } else if(action.type==='DELETE_STUDENT'){
+        console.log(action.type)
+        return state.filter((s)=>s.name!==action.payload)
+    }
+    else{
+        return state
+    }
+}
+// npm install --save redux-thunk
+
+
+// function fetchStudent(){
+//     return function(dispatch){
+//         fetch("http://bestlab.us:8080/students")
+//         .then(function(res){
+//             return res.json()
+//         })
+//         .then(function(data){
+//             dispatch({
+//                 type: 'FETCH_STUDENT_SUCCESS',
+//                 students: data
+//             })
+//         })   
+//     }
+// }
+
+function fetchStudent(){
+    return function(dispatch){
+        fetch('http://bestlab.us:8080/students')
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(data){
+            dispatch({
+                type: 'FETCH_STUDENT_SUCCESS',
+                payload: data
+            })
+        })
+    }
+}
+
+export function addStudent(student){
+    return function(dispatch){
+        fetch('http://bestlab.us:8080/students', {
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+              },
+            method: 'post', 
+            body: JSON.stringify(student)
+        })
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(data){
+            dispatch({
+                type: 'ADD_STUDENT',
+                payload: data
+            })
+        })
+    }
+}
+
+var centralState = combineReducers({
+    students
+})
+
+var logging = store => next => action => {
+    console.log(action.type)
+}
+
+var store = createStore(centralState, applyMiddleware(thunk))
+
+store.dispatch({type: 'LOAD_STUDENT'})
+store.dispatch({type: 'ADD_STUDENT', payload: {name: 'Tim'}})
+store.dispatch({type: 'DELELE_STUDENT', payload: {name: 'Thanh'}})
+store.dispatch(fetchStudent())
+
+
 
 ReactDOM.render(
-    <App/>, document.getElementById('app')
+<Provider store={store}>    
+    <App />
+</Provider>    
+    , document.getElementById('app')
 
 )
